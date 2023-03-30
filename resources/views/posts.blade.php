fe92b03fedf0464d6279d74b71812cc7aa9702dc
Taufik Hidayat <taufihidayat9503@gmail.com>
2023-03-31 05:07:30 +0700

    update data laravel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 <br>
 @foreach ($posts as $post)
 <article class="mb-5">
-    <h3><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h3>
+    <h3><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h3>
     <h5>By: {{ $post->author }}</h5>
     <p>{{ $post->excerpt }}</p> 
 </article>
