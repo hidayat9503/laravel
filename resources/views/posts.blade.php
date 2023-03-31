f2768df6037ebf2f9f302246174f4303065b6155
Taufik Hidayat <taufihidayat9503@gmail.com>
2023-04-01 06:58:09 +0700

    update data

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,9 @@
 <article class="mb-5 border-bottom py-3">
     <h3>{{ $post->title }}</h3>
     <p>By. <a class="text-decoration-none" href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->categories->slug }}"> {{ $post->categories->name }}</a></p>
-    <p>{{ $post->excerpt }} <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a></p> 
+    <p>{{ $post->excerpt }} <br> <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a></p> 
+    <small>Create: {{ $post->author->created_at }}</small>
+    
 </article>
 @endforeach
 
