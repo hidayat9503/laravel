f3acd687785253388adf4626dbf237f6db14a011
Taufik Hidayat <taufihidayat9503@gmail.com>
2023-04-01 20:58:40 +0700

    update post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,7 +8,7 @@
     <h3>{{ $post->title }}</h3>
     <p>By. <a class="text-decoration-none" href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a class="text-decoration-none" href="/categories/{{ $post->categories->slug }}"> {{ $post->categories->name }}</a></p>
     <p>{{ $post->excerpt }} <br> <a class="text-decoration-none" href="/posts/{{ $post->slug }}">Read more..</a></p> 
-    <small>Create: {{ $post->author->created_at }}</small>
+    <small>Create: {{ $post->author->created_at->format('F j, Y, g:i a') }}</small>
     
 </article>
 @endforeach
