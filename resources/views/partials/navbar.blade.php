796ea7461c7feb05a6420db41c62e7e3309deb7d
Taufik Hidayat <taufihidayat9503@gmail.com>
2023-04-03 13:03:27 +0700

    insert login

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,16 +2,29 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
     <div class="container">
       <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link {{ ($active === 'home')  ? 'active' : '' }}" aria-current="page" href="/">Home</a>
-          <a class="nav-link {{ ($active === 'about')  ? 'active' : '' }}" href="/about">About</a>
-          <a class="nav-link {{ ($active === 'categories')  ? 'active' : '' }}" href="/categories">Categories</a>
-          <a class="nav-link {{ ($active === 'posts')  ? 'active' : '' }}" href="/posts">Blog</a>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link {{ ($active === 'home')  ? 'active' : '' }}" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($active === 'about')  ? 'active' : '' }}" href="/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($active === 'categories')  ? 'active' : '' }}" href="/categories">Categories</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ ($active === 'posts')  ? 'active' : '' }}" href="/posts">Blog</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+              <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
