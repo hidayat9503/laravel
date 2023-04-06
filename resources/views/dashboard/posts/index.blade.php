519d6aead7f009e88eb8ebc2fb0c4a9653ac9b05
Taufik Hidayat <taufihidayat9503@gmail.com>
2023-04-06 20:54:22 +0700

    insert, edit, delete data laravel

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -5,7 +5,24 @@
     <h1 class="h2">{{ $title}}</h1>
 </div>
 
+@if (session()->has('success'))
+<div class="alert alert-success alert-dismissible fade show col-lg-8" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+  <strong>Success!</strong> {{ session('success') }}.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+
+@if (session()->has('deleted'))
+<div class="alert alert-danger alert-dismissible fade show col-lg-8" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  <strong>Success!</strong> {{ session('deleted') }}.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+@endif
+
 <div class="table-responsive col-lg-8">
+  <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create New Post</a>
     <table class="table table-striped table-sm">
       <thead>
         <tr>
@@ -23,8 +40,12 @@
           <td>{{ $post->categories->name }}</td>
           <td>
             <a class="badge bg-info" href="/dashboard/posts/{{ $post->slug }}"><span data-feather="eye"></span></a>
-            <a class="badge bg-warning" href="/dashboard/posts/{{ $post->id }}"><span data-feather="edit"></span></a>
-            <a class="badge bg-danger" href="/dashboard/posts/{{ $post->id }}"><span data-feather="x-circle"></span></a>
+            <a class="badge bg-warning" href="/dashboard/posts/{{ $post->slug }}/edit"><span data-feather="edit"></span></a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+              @method('delete')
+              @csrf
+              <button class="badge bg-danger border-0" onclick="return confirm('are you sure')"><span data-feather="x-circle"></span></button>
+            </form>  
           </td>
         </tr>
         @endforeach
